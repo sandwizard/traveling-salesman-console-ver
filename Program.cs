aba193fcd1e9c0c27822ae0f421cc8c1cdb49981
SandWizard <10zinjts@gmail.com>
2020-03-08 12:04:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using traveling_salesman_console_ver.Datamodel;
+
+namespace traveling_salesman_console_ver
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            DataModel dm = new DataModel();
+            
+        }
+    }
+}
